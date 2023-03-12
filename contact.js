3b29929833c7381e8a3fa5e18952a6b1f6cfd5f6
Dhiraj Naik <naikdhiraj9@gmail.com>
2023-03-12 21:28:35 +0530

    added contact credentials

--- contact.js
+++ contact.js
@@ -1,11 +1,24 @@
 function sendEmail() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let contact = document.getElementById("phone").value;
+  let subject = document.getElementById("message").value;
+
+  let body =
+    "Name: " +
+    name +
+    "<br/> Email: " +
+    email +
+    "<br/>Phone: " +
+    contact +
+    "<br/> message: " +
+    subject;
+
   Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "courseasus@gmail.com",
-    Password: "3BFF12F02E25F819E131FA1648BB282F3AC4",
+    SecureToken: "0479a533-1082-4f43-8146-6f674d96e64f",
     To: "naikdhiraj9@gmail.com",
-    From: document.getElementById("email").value,
+    From: "courseasus@gmail.com",
     Subject: "This is the subject",
-    Body: "And this is the body",
+    Body: body,
   }).then((message) => alert(message));
 }
